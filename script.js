125dab9518c9cf0737486b68b2bc814019fe1a39
Renu Shinde <renunikhilp@gmail.com>
2021-11-24 17:19:46 -0800

    sticky nav-bar and toggle nav-bar

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+// add fixed class to navbar
 window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
 });
-
-function toggleMenu() {
-  const menuToggle = document.querySelector(".menu-toggle");
-  menuToggle.classList.toggle("active");
-}
+// show sidebar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
